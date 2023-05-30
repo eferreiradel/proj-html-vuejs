0f717059cd81ca43d76fe3785fa92f227e89ea14
eferreiradel <cont.limaferreira.everton@gmail.com>
2023-05-30 13:27:44 +0200

    Initial commit: Project scaffold Dependencies -Tailwind Css -Font Awesome

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <div class="bg-blue-500 text-white p-4">
+    <h1 class="text-2xl">Hello Tailwind CSS!</h1>
+  </div>
+  <div>
+    <i class="fa-solid fa-magnifying-glass"></i>
+  </div>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
