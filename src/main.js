4760e6dd29c1a7b3281ce6edd83258557ffe72c1
eferreiradel <cont.limaferreira.everton@gmail.com>
2023-05-30 14:05:12 +0200

    Add:  Dependencies: - Sass  Files: Store.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from "vue";
-import "./style.css";
+import "./styles/main.scss";
 import App from "./App.vue";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "../index.css";
