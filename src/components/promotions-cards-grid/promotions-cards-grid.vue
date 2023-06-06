2feccbdf20a133b5c9e45619eb6e1b2cf8f17a22
eferreiradel <cont.limaferreira.everton@gmail.com>
2023-06-06 15:09:48 +0200

    style: cards animated

--- src/components/promotions-cards-grid/promotions-cards-grid.vue
+++ src/components/promotions-cards-grid/promotions-cards-grid.vue
@@ -5,14 +5,22 @@
         <div class="col-10 px-0">
           <div class="container-fluid p-0">
             <div class="row card-grid py-0 mx-0">
-              <div class="col-4 px-3" v-for="card in cards">
-                <div class="img-container">
-                  <img :src="card.image" />
-                </div>
-                <div class="container py-4 text-center">
-                  <span class="subheading--dark">
-                    {{ card.caption }}
-                  </span>
+              <div class="col-4 card--container px-3" v-for="card in cards">
+                <div class="content--container">
+                  <div class="shade">
+                    <div class="img-container">
+                      <div class="container text-center p-0">
+                        <i class="fa-solid fa-link"></i>
+                      </div>
+                      <a class="subheading--light">{{ card.caption }}</a>
+                    </div>
+                    <img :src="card.image" />
+                  </div>
+                  <div class="link--container py-4 text-center">
+                    <span class="subheading--dark">
+                      {{ card.caption }}
+                    </span>
+                  </div>
                 </div>
               </div>
             </div>
@@ -50,7 +58,50 @@ export default {
 };
 </script>
 <style scoped lang="scss">
-.img-container {
-  height: 30rem;
+@import "../../styles/variables.scss";
+
+.content--container {
+  position: relative;
+  height: 40em;
+}
+.shade {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  opacity: 0;
+  z-index: 1;
+  transition: 0.5s;
+  i {
+    font-size: 1.5rem;
+    color: $color-secondary;
+  }
+}
+.card--container {
+  &:hover {
+    .img-container {
+      height: 100%;
+    }
+    .shade {
+      opacity: 1;
+    }
+  }
+
+  .img-container {
+    position: relative;
+    z-index: 1;
+    width: 100%;
+    height: 85%;
+    transition: height 0.2s ease-in;
+  }
+
+  .link--container {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    padding: 0;
+    margin: 0;
+  }
 }
 </style>
